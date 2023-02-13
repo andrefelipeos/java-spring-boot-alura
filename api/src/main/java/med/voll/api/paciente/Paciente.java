c678a46bcd1a4eeec2f5649e859281892782e74a
André Sousa <andre.lipe.o@gmail.com>
2023-02-13 18:45:31 -0300

    feat: add constructor that uses DTO in entity 'Paciente'

--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -31,4 +31,12 @@ public class Paciente {
 	@Embedded
 	private Endereco endereco;
 
+	public Paciente(DadosCadastroPaciente dados) {
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.cpf = dados.cpf();
+		this.endereco = new Endereco(dados.endereco());
+	}
+
 }
