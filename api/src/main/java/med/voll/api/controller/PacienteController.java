37994112f85a2f1381cdb01dac77dd7926ed14be
André Sousa <andre.lipe.o@gmail.com>
2023-02-13 19:04:58 -0300

    fix: add annotations to the 'adicionar' method
    Adds @Transactional and @Valid annotations

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,11 +1,13 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
@@ -18,7 +20,8 @@ public class PacienteController {
 	private PacienteRepository repository;
 
 	@PostMapping
-	public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
+	@Transactional
+	public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
 		repository.save(new Paciente(dados));
 	}
 
