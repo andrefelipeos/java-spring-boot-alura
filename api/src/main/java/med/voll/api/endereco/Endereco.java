a1868d2ca80d63bab0655b8725c6e6053329de17
André Sousa <andre.lipe.o@gmail.com>
2023-02-15 15:18:04 -0300

    feat: create DTO for address update

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -29,7 +29,7 @@ public class Endereco {
 		this.uf = dados.uf();
 	}
 
-	public void atualizar(DadosEndereco dados) {
+	public void atualizar(DadosAtualizacaoEndereco dados) {
 		if (dados.logradouro() != null) this.logradouro = dados.logradouro();
 		if (dados.bairro() != null) this.bairro = dados.bairro();
 		if (dados.cep() != null) this.cep = dados.cep();
--- api/src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -1,13 +1,15 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.endereco.DadosAtualizacaoEndereco;
 
 public record DadosAtualizacaoMedico(
 		@NotNull
 		Long identificador,
 		String nome,
 		String telefone,
-		DadosEndereco endereco) {
+		@Valid
+		DadosAtualizacaoEndereco endereco) {
 
 }
