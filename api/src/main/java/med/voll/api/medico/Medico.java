04fe47183ea7dae76d78616bb87f3dbde87f48b1
André Sousa <andre.lipe.o@gmail.com>
2023-02-15 14:45:37 -0300

    feat: implement a method to update entities 'Medico'

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -46,6 +47,12 @@ public class Medico {
 		this.crm = dados.crm();
 		this.especialidade = dados.especialidade();
 		this.endereco = new Endereco(dados.endereco());
+	}
+
+	public void atualizar(@Valid DadosAtualizacaoMedico dados) {
+		if (dados.nome() != null) this.nome = dados.nome();
+		if (dados.telefone() != null) this.telefone = dados.telefone();
+		if (dados.endereco() != null) this.endereco.atualizar(dados.endereco());
 	}
 
 }
