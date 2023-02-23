1b30296cccee6ef8c62b676933c135648609b964
André Sousa <andre.lipe.o@gmail.com>
2023-02-23 17:41:08 -0300

    refactor: return entities and '201 created' code on methods 'cadastrar'

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosAtualizacaoPaciente;
@@ -32,8 +33,11 @@ public class PacienteController {
 
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
-		repository.save(new Paciente(dados));
+	public ResponseEntity<DadosDetalhamentoPaciente> cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
+		var paciente = new Paciente(dados);
+		repository.save(paciente);
+		var uri = uriBuilder.path("/pacientes/{identificador}").buildAndExpand(paciente.getIdentificador()).toUri();
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
 	}
 
 	@GetMapping("/{identificador}")
