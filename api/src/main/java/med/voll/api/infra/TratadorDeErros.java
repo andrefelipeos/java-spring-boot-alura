2917c54ee7c68bac9933d413d13e0fdb6843cfa4
André Sousa <andre.lipe.o@gmail.com>
2023-02-24 18:01:11 -0300

    feat: return custom 'bad request' messages on invalid arguments

--- api/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,6 +1,10 @@
 package med.voll.api.infra;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,9 +13,21 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class TratadorDeErros {
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+	}
+
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<Void> tratarErro404() {
 		return ResponseEntity.notFound().build();
+	}
+
+	private record DadosErroValidacao(String campo, String mensagem) {
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
 	}
 
 }
