c971ea919ed69a7adda919ec7370b307bd64cba9
André Felipe <andre.felipe.2048@gmail.com>
2023-02-27 20:13:23 -0300

    feat: create entity 'Usuario'

---
+++ api/src/main/java/med/voll/api/usuario/Usuario.java
@@ -0,0 +1,32 @@
+package med.voll.api.usuario;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "identificador")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long identificador;
+
+	@Column(name = "usuario")
+	private String nomeDeUsuario;
+
+	private String senha;
+
+}
