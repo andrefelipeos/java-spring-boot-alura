56449aa9c376642577191aa80499224d4d15d891
André Felipe <andre.felipe.2048@gmail.com>
2023-03-13 22:10:01 -0300

    feat: validate existence of sent ids

--- api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -3,6 +3,7 @@ package med.voll.api.domain.consulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 
@@ -19,6 +20,14 @@ public class AgendaDeConsultas {
 	private PacienteRepository pacienteRepository;
 
 	public void agendar(DadosAgendamentoConsulta dados) {
+		if (!pacienteRepository.existsById(dados.idDoPaciente())) {
+			throw new ValidacaoException("Identificador de paciente informado não existe");
+		}
+
+		if (dados.idDoMedico() != null && !medicoRepository.existsById(dados.idDoMedico())) {
+			throw new ValidacaoException("Identificador de médico informado não existe");
+		}
+
 		var medico = medicoRepository.findById(dados.idDoMedico()).get();
 		var paciente = pacienteRepository.findById(dados.idDoPaciente()).get();
 		var consulta = new Consulta(null, medico, paciente, dados.horarioComData());
