64a9f7bb6eea0ee5fee1f1de37e2dc53aa58ac4b
André Felipe <andre.felipe.2048@gmail.com>
2023-03-14 19:11:29 -0300

    feat: choose a random 'Medico' when none is selected

--- api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 
@@ -28,10 +29,22 @@ public class AgendaDeConsultas {
 			throw new ValidacaoException("Identificador de médico informado não existe");
 		}
 
-		var medico = medicoRepository.findById(dados.idDoMedico()).get();
+		var medico = escolherMedico(dados);
 		var paciente = pacienteRepository.findById(dados.idDoPaciente()).get();
 		var consulta = new Consulta(null, medico, paciente, dados.horarioComData());
 		consultaRepository.save(consulta);
+	}
+
+	private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+		if (dados.idDoMedico() != null) {
+			return medicoRepository.getReferenceById(dados.idDoMedico());
+		}
+
+		if (dados.especialidade() == null) {
+			throw new ValidacaoException("Campo Especialidade é obrigatório quando o médico não é selecionado");
+		}
+
+		return medicoRepository.escolherMedicoAleatorioLivreNoHorario(dados.especialidade(), dados.horarioComData());
 	}
 
 }
