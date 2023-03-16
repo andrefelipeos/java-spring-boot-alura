37defb7727a2f0acd8e8d1b711584a0f7962e1ab
André Felipe <andre.felipe.2048@gmail.com>
2023-03-16 15:47:51 -0300

    feat: create interface 'ValidacaoAgendamentoConsulta'

---
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoAgendamentoConsulta.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public interface ValidacaoAgendamentoConsulta {
+
+	void validar(DadosAgendamentoConsulta dados);
+
+}
