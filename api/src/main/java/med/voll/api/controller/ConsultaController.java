a8ce4d050ad496163c4a9b3475b3e75ff8e2acb9
André Felipe <andre.felipe.2048@gmail.com>
2023-05-03 21:20:39 -0300

    feat: add HTTP DELETE method 'cancelar' in class 'ConsultaController'

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 
 @RestController
@@ -27,4 +29,10 @@ public class ConsultaController {
 		return ResponseEntity.ok(dto);
 	}
 
+	@DeleteMapping
+	@Transactional
+	public ResponseEntity<Void> cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+	    agenda.cancelar(dados);
+	    return ResponseEntity.noContent().build();
+	}
 }
